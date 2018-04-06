d9f3d2ba028a9464091cdf6cb3d873ddab9cff30
Justin Littman <justinlittman@gwu.edu>
2018-04-06 15:16:27 -0400

    Improvement to urls.py.

--- utils/urls.py
+++ utils/urls.py
@@ -8,12 +8,28 @@ from __future__ import print_function
 import json
 import fileinput
 
-for line in fileinput.input():
-    tweet = json.loads(line)
-    for url in tweet["entities"]["urls"]:
-        if 'unshortened_url' in url:
-            print(url['unshortened_url'].encode('utf8'))
-        elif url.get('expanded_url'):
-            print(url['expanded_url'].encode('utf8'))
-        elif url.get('url'):
-            print(url['url'].encode('utf8'))
+
+def extract_tweets(tweet):
+    tweets = list()
+    tweets.append(tweet)
+    if 'quoted_status' in tweet:
+        tweets.extend(extract_tweets(tweet['quoted_status']))
+    elif 'retweeted_status' in tweet:
+        tweets.extend(extract_tweets(tweet['retweeted_status']))
+    return tweets
+
+
+def extract_entity_urls(tweet):
+    urls = set()
+    entities = tweet.get('extended_tweet', {}).get('entities') or tweet.get('entities', {})
+    for url_sect in entities.get('urls', []):
+        if 'expanded_url' in url_sect:
+            urls.add(url_sect['expanded_url'].encode('utf8'))
+    return urls
+
+
+if __name__ == '__main__':
+    for line in fileinput.input():
+        for tweet in extract_tweets(json.loads(line)):
+            for url in extract_entity_urls(tweet):
+                print(url)
